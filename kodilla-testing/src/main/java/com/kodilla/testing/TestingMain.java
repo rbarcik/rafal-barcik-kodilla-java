cdf3dbcfd29807c8d6b5786015a7f68b8406d278
rbarcik <barcik.r@gmail.com>
2018-11-12 23:15:10 +0100

    Commit3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(5,7);
+
+        if(result == 12){
+            System.out.println("Test add ok");
+        } else {
+            System.out.println("Add error!");
+        }
+
+        result = calculator.substract(7,5);
+
+        if(result == 2){
+            System.out.println("Test substract ok");
+        } else {
+            System.out.println("Substract error!");
+        }
     }
 }

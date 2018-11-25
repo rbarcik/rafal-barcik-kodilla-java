53dcfe77022a0957097a788f1b876c3c60c6e61a
rbarcik <barcik.r@gmail.com>
2018-11-25 14:31:41 +0100

    7.3._1 Poznanie funkcji stream(), filter(), map() i collect()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,21 +4,43 @@ package com.kodilla.stream;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
 public class StreamMain {
     public static void main(String[] args) {
+        //Joining
+        System.out.println("Collectors Joining");
         BookDirectory theBookDirectory = new BookDirectory();
         String theResultStringOfBooks = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
                 .map(Book::toString)
                 .collect(Collectors.joining(",\n","<<",">>"));
-
-
-
         System.out.println(theResultStringOfBooks);
 
+        //Collectors toMap
+        System.out.println("\n Collectors toMap");
+        BookDirectory theBookDirectory1 = new BookDirectory();
+        Map<String, Book> theResultMapOfBooks = theBookDirectory1.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
 
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        //Collectors toLIst
+        System.out.println("\n Collectors toList");
+        BookDirectory theBookDirectory2 = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory2.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
     }
 }

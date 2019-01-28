61ad32f93d79f0e62c582f0242524883220cbc22
rbarcik <barcik.r@gmail.com>
2019-01-28 18:42:44 +0100

    Exercise: 14.2 Kalkulator z wyświetlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,54 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.add(5, 7);
+        //Then
+        Assert.assertEquals(12, result, 0);
+    }
+    @Test
+    public void testSub() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.sub(7, 5);
+        //Then
+        Assert.assertEquals(2, result, 0);
+    }
+    @Test
+    public void testMul() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.mul(7, 5);
+        //Then
+        Assert.assertEquals(35, result, 0);
+    }
+    @Test
+    public void testDiv() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double result = calculator.div(7, 0);
+        //Then
+        Assert.assertEquals(0, result, 0);
+    }
+}

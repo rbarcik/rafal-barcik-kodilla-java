9daddf97fd429e44c1baeebaa89865b437c0cd35
rbarcik <barcik.r@gmail.com>
2019-01-30 09:50:48 +0100

    14.3. Konfiguracja przy pomocy klasy Java

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/reader/Book.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.reader;
+
+public final class Book {
+    final String tittle;
+
+    public Book(final String tittle) {
+        this.tittle = tittle;
+    }
+    public String getTittle() {
+        return tittle;
+    }
+}
